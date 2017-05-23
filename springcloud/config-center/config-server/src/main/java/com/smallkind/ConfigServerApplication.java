9f3b51efcaab1c639ae5643a53bb8549447ef09c
xiang <xiang.zhou@criwell.com>
2017-05-23 16:17:51 +0800

    test config center

---
+++ springcloud/config-center/config-server/src/main/java/com/smallkind/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.smallkind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer //开启Config Server
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+	}
+}
