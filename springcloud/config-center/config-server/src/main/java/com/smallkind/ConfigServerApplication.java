2d365aa0ed5a9c620451fe7c1132931ca93f713e
xiang <xiang.zhou@criwell.com>
2017-05-24 21:32:58 +0800

    TODO: 分布式配置中心

--- springcloud/config-center/config-server/src/main/java/com/smallkind/ConfigServerApplication.java
+++ springcloud/config-center/config-server/src/main/java/com/smallkind/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.smallkind;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
