760e1c13b600b853e7135ca9189a1606abbb7904
zack.zhou <zack.zhou@jfz.com>
2017-06-28 12:41:46 +0800

    【添加】测试函数接口传递Lambda表达式

--- springboot/java8/src/test/java/com/smallkind/lambda/LambdaTest.java
+++ springboot/java8/src/test/java/com/smallkind/lambda/LambdaTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +23,12 @@ public class LambdaTest {
 
     @Test
     public void testHiddenFiles(){
-        System.out.println(Lambda.hiddenFiles("/Users/zhouxiang/Documents/").length);
+        System.out.println(Lambda.hiddenFiles("/Users/admin/Documents/").length);
     }
 
     @Test
     public void testHiddenFilesByJava8(){
-        System.out.println(Lambda.hiddenFilesByJava8("/Users/zhouxiang/Documents/").length);
+        System.out.println(Lambda.hiddenFilesByJava8("/Users/admin/Documents/").length);
     }
 
     @Test
@@ -39,13 +40,26 @@ public class LambdaTest {
         r.run();
     }
 
-    @Test
-    public void testTry(){
+    /** 如何利用函数表达式传递Lambda start **/
+    @FunctionalInterface
+    interface BufferedReaderProcessor{
+        String proccess(BufferedReader bufferedReader) throws IOException;
+    }
+
+    public static String processFile(BufferedReaderProcessor bufferedReaderProcessor) throws IOException{
         //Java7带资源的try语句，不需要显示的关闭资源
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\test\\测试.pdf"))) {
-            bufferedReader.readLine();
-        }catch (Exception e){
-            e.printStackTrace();
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\admin\\Desktop\\aa.txt"))) {
+            return bufferedReaderProcessor.proccess(bufferedReader);
         }
     }
+
+    @Test
+    public void testprocessFile() throws IOException{
+        String oneLine = processFile((BufferedReader bufferedReader) -> bufferedReader.readLine());
+        System.out.println(oneLine);
+        String twoLine = processFile((BufferedReader bufferedReader) -> bufferedReader.readLine() + "\n" + bufferedReader.readLine());
+        System.out.println(twoLine);
+    }
+    /** 如何利用函数表达式传递Lambda end **/
+
 }
