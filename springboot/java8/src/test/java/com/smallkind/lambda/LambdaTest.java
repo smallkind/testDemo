92e5537b56d637cd0efe171d7bf1a30ae77f87a7
xiang <xiang.zhou@criwell.com>
2017-06-28 20:50:12 +0800

    【添加】测试stream

--- springboot/java8/src/test/java/com/smallkind/lambda/LambdaTest.java
+++ springboot/java8/src/test/java/com/smallkind/lambda/LambdaTest.java
@@ -5,6 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author smallkind
  * @description
@@ -23,5 +28,59 @@ public class LambdaTest {
     @Test
     public void testHiddenFilesByJava8(){
         System.out.println(Lambda.hiddenFilesByJava8("/Users/zhouxiang/Documents/").length);
+    }
+
+    @Test
+    public void testList(){
+        List<Apple> list = Arrays.asList(
+                new Apple(80,"green"),
+                new Apple(155, "green"),
+                new Apple(120, "red"),
+                new Apple(120, "blue"),
+                new Apple(140, "block"),
+                new Apple(190, "write"),
+                new Apple(120,"red"),
+                null);
+        List<Integer> list1 = list.stream()
+                .filter(apple -> apple != null && apple.getWeight() != 0 )
+                .map(Apple::getWeight)
+                .collect(Collectors.toList());
+        System.out.println("集合的大小为：" + list1.size());
+        for(Integer integer : list1){
+            System.out.println(integer);
+        }
+    }
+
+    public static class Apple {
+        private int weight = 0;
+        private String color = "";
+
+        public Apple(int weight, String color){
+            this.weight = weight;
+            this.color = color;
+        }
+
+        public Integer getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Integer weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String toString() {
+            return "Apple{" +
+                    "color='" + color + '\'' +
+                    ", weight=" + weight +
+                    '}';
+        }
     }
 }
