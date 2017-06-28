3c542a6b01350b5be1f0e042dd4e5fec65efcf03
zack.zhou <zack.zhou@jfz.com>
2017-06-28 09:36:05 +0800

    【添加】测试Lambda表达式，带资源try语句

--- springboot/java8/src/test/java/com/smallkind/lambda/LambdaTest.java
+++ springboot/java8/src/test/java/com/smallkind/lambda/LambdaTest.java
@@ -5,6 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author smallkind
  * @description
@@ -23,5 +28,24 @@ public class LambdaTest {
     @Test
     public void testHiddenFilesByJava8(){
         System.out.println(Lambda.hiddenFilesByJava8("/Users/zhouxiang/Documents/").length);
+    }
+
+    @Test
+    public void testListIsEmpty(){
+      process(()-> System.out.println("hello world!"));
+    }
+
+    public static void process(Runnable r){
+        r.run();
+    }
+
+    @Test
+    public void testTry(){
+        //Java7带资源的try语句，不需要显示的关闭资源
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\test\\测试.pdf"))) {
+            bufferedReader.readLine();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
