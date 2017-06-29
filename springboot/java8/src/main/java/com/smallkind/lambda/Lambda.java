1ae6a46bb7ddfaaee9586e30b8f3afca2cb7a01c
zack.zhou <zack.zhou@jfz.com>
2017-06-29 09:30:46 +0800

    【新增】测试predicate,consumer,function接口

--- springboot/java8/src/main/java/com/smallkind/lambda/Lambda.java
+++ springboot/java8/src/main/java/com/smallkind/lambda/Lambda.java
@@ -1,6 +1,11 @@
 package com.smallkind.lambda;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author smallkind
@@ -45,4 +50,52 @@ public class Lambda {
         }
     }
     /** 如何利用函数表达式传递Lambda end **/
+
+    /**
+     * Predicate 接受泛型T，返回Boolean
+     * boolean test(T t)
+     * @param list
+     * @param predicate
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> filter(List<T> list, Predicate<T> predicate){
+        List<T> results = new ArrayList<>();
+        for(T t : list){
+            if(predicate.test(t)){
+                results.add(t);
+            }
+        }
+        return results;
+    }
+
+    /**
+     * Consumer 接受泛型T，返回void
+     * void accept(T t)
+     * @param list
+     * @param consumer
+     * @param <T>
+     */
+    public static <T> void forEach(List<T> list, Consumer<T> consumer){
+        for(T t : list){
+            consumer.accept(t);
+        }
+    }
+
+    /**
+     * Function 接受泛型T，返回泛型R
+     * R apply(T t)
+     * @param list
+     * @param function
+     * @param <T>
+     * @param <R>
+     * @return
+     */
+    public static <T,R> List<R> map(List<T> list, Function<T,R> function){
+        List<R> results = new ArrayList<>();
+        for(T t : list){
+            results.add(function.apply(t));
+        }
+        return results;
+    }
 }
